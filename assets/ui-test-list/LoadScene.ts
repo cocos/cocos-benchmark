424612c4e83f8910efebcbb95e56eb8641b91b29
ChiaNing <sun.cloud.mo@gmail.com>
2021-06-09 16:01:54 +0800

    add test list

---
+++ assets/ui-test-list/LoadScene.ts
@@ -0,0 +1,30 @@
+
+import { _decorator, Component, Node, director } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('LoadScene')
+export class LoadScene extends Component {
+    public loadBunny () {
+        director.loadScene('BunnyTest');
+    }
+    public loadFrame () {
+        director.loadScene('BunnyFrameAnimationTest');
+    }
+    public loadTransform () {
+        director.loadScene('BunnyTransformAnimationTest');
+    }
+    public loadUI () {
+        director.loadScene('UITest');
+    }
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.0/manual/en/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.0/manual/en/scripting/ccclass.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.0/manual/en/scripting/life-cycle-callbacks.html
+ */
