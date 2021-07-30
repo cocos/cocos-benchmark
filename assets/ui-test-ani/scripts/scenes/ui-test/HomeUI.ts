e532659723b939c56cf4a7884cd33b47c78bdf6c
PP_Pro <chinese1236@qq.com>
2021-07-30 19:03:24 +0800

    移除 eventManager 使用

--- assets/ui-test-ani/scripts/scenes/ui-test/HomeUI.ts
+++ assets/ui-test-ani/scripts/scenes/ui-test/HomeUI.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, Animation, Enum, eventManager } from "cc";
+import { _decorator, Component, Node, Animation, Enum } from "cc";
 const { ccclass, property } = _decorator;
 import { BackPackUIComp } from "./BackPackUIComp";
 import { ShopUI } from "./ShopUI";
@@ -37,17 +37,6 @@ export class HomeUI extends Component {
         this.homeBtnGroups[0].getChildByName("sub_btns").getComponent("SubBtnsUI").showSubBtns();
         this.node.parent.getChildByName("chargePanel").getComponent("ChargeUI").show();
         this.node.parent.getChildByName("backPack").getComponent("BackPackUIComp").show();
-    }
-
-    toggleHomeBtns(enable: boolean) {
-        for (let i = 0; i < this.homeBtnGroups.length; ++i) {
-            let group = this.homeBtnGroups[i];
-            if (!enable) {
-                eventManager.pauseTarget(group, true);
-            } else {
-                eventManager.resumeTarget(group, true);
-            }
-        }
     }
 
     gotoShop() {
