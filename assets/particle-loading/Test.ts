81ec32fb86b3b4eb78e42ad734f3fa4db8cd8d30
liuyi <liuyi@cocos.com>
2022-08-22 16:55:06 +0800

    delete redundant meta

--- assets/particle-loading/Test.ts
+++ assets/particle-loading/Test.ts
@@ -18,11 +18,7 @@ const { ccclass, property } = cc._decorator;
 export class Test extends cc.Component {
 
     private label: cc.Label;
-    private label1: cc.Label;
-    private label10: cc.Label;
-
     private nodePool: cc.NodePool;
-
     private elapse: number = 0;
 
     start() {
@@ -50,7 +46,6 @@ export class Test extends cc.Component {
 
     load() {
         let time = Date.now();
-
         cc.resources.load("LordLavaArea", cc.Prefab, null, (error, asset) => {
             if (error) {
                 console.error('load asset failed', error);
@@ -74,15 +69,10 @@ export class Test extends cc.Component {
         // wx.triggerGC();
     }
 
-    update(dt) {
-        // this.elapse += dt;
-        // this.label1.string = "时间：" + this.elapse;
-    }
+    update() {}
 
     load10() {
         let time = Date.now();
-        let sumTime = 0;
-
         for (let n = 1; n < 11; ++n) {
             cc.resources.load("LordLavaArea", cc.Prefab, null, (error, asset) => {
                 if (error) {
@@ -96,15 +86,9 @@ export class Test extends cc.Component {
 
                 this.label.string = "池大小：" + this.nodePool.size() + "\n累计" +  n + "次耗时：" + (Date.now() - time) + "\n平均耗时：" + ((Date.now() - time)/n).toFixed(2);
                 console.log("池大小：" + this.nodePool.size() + "  累计" +  n + "次耗时：" + (Date.now() - time) + "  平均耗时：" + ((Date.now() - time)/n).toFixed(2))
-                sumTime += (Date.now() - time);
             });
         }
-
-        // this.label.string = "池大小：" + this.nodePool.size() + " 耗时：" + (Date.now() - time + "平均耗时：" + sumTime/10);
-        // console.log("池大小：" + this.nodePool.size() + " 耗时：" + (Date.now() - time) + "平均耗时：" + sumTime/10)
-
     }
-
 }
 
 /**
